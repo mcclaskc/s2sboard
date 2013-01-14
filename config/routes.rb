610857b2104d10fde908d65bc4e8ccc0cb89939f
Fletcher Richman <frichman3@gmail.com>
2013-01-14 00:10:52 -0700

    first stab at homepage controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 S2sboard::Application.routes.draw do
+  get "welcome/index"
+
   resources :jobs
 
   resources :users
@@ -52,11 +54,14 @@ S2sboard::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  
+  
 end
+
