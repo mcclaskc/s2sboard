9c50eaf4a778ffeb62628c2a43ef98e1218aa01c
Koma <koma@ubuntu.(none)>
2013-01-15 19:43:39 -0700

    first sessions commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
-  attr_accessible :company, :email, :location, :name, :password
+  attr_accessible :company, :email, :location, :name, :password, :password_confirmation
+  has_secure_password
+
+  before_save {|user| user.email = email.downcase}
+  before_save :create_remember_token
+
+  validates :password, length: {minimum: 6}
+  validates :password_confirmation, presence: true
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
