227a5966678be011e0c0b77e8f1976dd3793b001
Chris McClaskey <christopher.m.mcclaskey@gmail.com>
2013-01-22 20:34:52 -0700

    added some jobs to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+jobs = Job.create([
+		{
+			name: 'Rails Dev'
+	    	type: 'Internship'
+	    	description: 'Do some Rails shit'
+	    	user_id: 1
+	    	contact: 'user1@domain'
+		}
+		{
+			name: 'Googling (a little) Shit'
+	    	type: 'Internship'
+	    	description: 'Practice Googling shit'
+	    	user_id: 2
+	    	contact: 'user2@domain'
+		}
+		{
+			name: 'Pro Googler'
+	    	type: 'Full Time'
+	    	description: 'Google Shit Full time'
+	    	user_id: 3
+	    	contact: 'user3@domain'
+		}
+		{
+			name: 'App Maker'
+	    	type: 'Part Time'
+	    	description: 'Make some dope apps n shit with some dude'
+	    	user_id: 4
+	    	contact: 'user4@domain'
+		}
+
+	])
