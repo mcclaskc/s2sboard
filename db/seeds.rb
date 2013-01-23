0dd5349e53eba8c562e485b3047dd847dd64bacb
Chris McClaskey <christopher.m.mcclaskey@gmail.com>
2013-01-22 20:38:14 -0700

    syntax fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,31 +9,31 @@
 
 jobs = Job.create([
 		{
-			name: 'Rails Dev'
-	    	type: 'Internship'
-	    	description: 'Do some Rails shit'
-	    	user_id: 1
+			name: 'Rails Dev',
+	    	type: 'Internship',
+	    	description: 'Do some Rails shit',
+	    	user_id: 1,
 	    	contact: 'user1@domain'
-		}
+		},
 		{
-			name: 'Googling (a little) Shit'
-	    	type: 'Internship'
-	    	description: 'Practice Googling shit'
-	    	user_id: 2
+			name: 'Googling (a little) Shit',
+	    	type: 'Internship',
+	    	description: 'Practice Googling shit',
+	    	user_id: 2,
 	    	contact: 'user2@domain'
-		}
+		},
 		{
-			name: 'Pro Googler'
-	    	type: 'Full Time'
-	    	description: 'Google Shit Full time'
-	    	user_id: 3
+			name: 'Pro Googler',
+	    	type: 'Full Time',
+	    	description: 'Google Shit Full time',
+	    	user_id: 3,
 	    	contact: 'user3@domain'
-		}
+		},
 		{
-			name: 'App Maker'
-	    	type: 'Part Time'
-	    	description: 'Make some dope apps n shit with some dude'
-	    	user_id: 4
+			name: 'App Maker',
+	    	type: 'Part Time',
+	    	description: 'Make some dope apps n shit with some dude',
+	    	user_id: 4,
 	    	contact: 'user4@domain'
 		}
 
