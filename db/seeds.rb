25b9f1c2a6eef92b7d83ec602676c42197096240
Chris McClaskey <christopher.m.mcclaskey@gmail.com>
2013-01-28 08:47:51 -0700

    commented out seeds for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,34 +7,34 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-jobs = Job.create([
-		{
-			name: 'Rails Dev',
-	    	job_type: 'Internship',
-	    	description: 'Do some Rails shit',
-	    	user_id: 1,
-	    	contact: 'user1@domain'
-		},
-		{
-			name: 'Googling (a little) Shit',
-	    	job_type: 'Internship',
-	    	description: 'Practice Googling shit',
-	    	user_id: 2,
-	    	contact: 'user2@domain'
-		},
-		{
-			name: 'Pro Googler',
-	    	job_type: 'Full Time',
-	    	description: 'Google Shit Full time',
-	    	user_id: 3,
-	    	contact: 'user3@domain'
-		},
-		{
-			name: 'App Maker',
-	    	job_type: 'Part Time',
-	    	description: 'Make some dope apps n shit with some dude',
-	    	user_id: 4,
-	    	contact: 'user4@domain'
-		}
+# jobs = Job.create([
+# 		{
+# 			name: 'Rails Dev',
+# 	    	job_type: 'Internship',
+# 	    	description: 'Do some Rails shit',
+# 	    	user_id: 1,
+# 	    	contact: 'user1@domain'
+# 		},
+# 		{
+# 			name: 'Googling (a little) Shit',
+# 	    	job_type: 'Internship',
+# 	    	description: 'Practice Googling shit',
+# 	    	user_id: 2,
+# 	    	contact: 'user2@domain'
+# 		},
+# 		{
+# 			name: 'Pro Googler',
+# 	    	job_type: 'Full Time',
+# 	    	description: 'Google Shit Full time',
+# 	    	user_id: 3,
+# 	    	contact: 'user3@domain'
+# 		},
+# 		{
+# 			name: 'App Maker',
+# 	    	job_type: 'Part Time',
+# 	    	description: 'Make some dope apps n shit with some dude',
+# 	    	user_id: 4,
+# 	    	contact: 'user4@domain'
+# 		}
 
-	])
+# 	])
