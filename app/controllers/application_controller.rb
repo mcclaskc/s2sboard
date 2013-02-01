1f304c6d218ef98e6c5b75225930c1e66e26f76e
Fletcher Richman <frichman3@gmail.com>
2013-02-01 00:51:16 -0700

    not sure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
   include JobsHelper
+
+  	def not_found
+  		raise ActionController::RoutingError.new('Not Found')
+	end	
 end
