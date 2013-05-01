2a479bce9cd254645d52d58fedd67d502cc50cc6
Fletcher Richman <frichman3@gmail.com>
2013-05-01 00:36:59 -0600

    switched order

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,6 +8,6 @@ class WelcomeController < ApplicationController
 
   def check_params 	
   	 @order_by = Job.column_names.include?(params[:order_by]) || params[:order_by] == 'company' ? params[:order_by] : "created_at"
-  	 @direction =  params[:direction] == 'asc' || params[:direction] == 'desc' ? params[:direction] : "asc"
+  	 @direction =  params[:direction] == 'desc' || params[:direction] == 'asc' ? params[:direction] : "desc"
   end
 end
